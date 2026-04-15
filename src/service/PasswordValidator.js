807616658ece9eadbb3b131d2ef0f358615f22a2
RomuloJB <jordaobogoni@gmail.com>
2026-04-15 00:55:37 -0300

    add | adiciona validação de letra minúscula

--- src/service/PasswordValidator.js
+++ src/service/PasswordValidator.js
@@ -2,24 +2,31 @@ const { constantes } = require('../util/Constantes');
 const { mensagens } = require('../util/Mensagens');
 
 class PasswordValidator {
-  static validar(senha) {
-    this.validarTamanhoMinimo(senha);
-    this.validarMaiuscula(senha);
+    static validar(senha) {
+        this.validarTamanhoMinimo(senha);
+        this.validarMaiuscula(senha);
+        this.validarMinuscula(senha);
 
     return true;
-  }
-
-  static validarTamanhoMinimo(senha) {
-    if (senha.length < constantes.SENHA_TAMANHO_MINIMO) {
-      throw new Error(mensagens.SENHA_MUITO_CURTA);
     }
-  }
 
-  static validarMaiuscula(senha) {
-    if (!/[A-Z]/.test(senha)) {
-      throw new Error(mensagens.SENHA_SEM_MAIUSCULA);
+    static validarTamanhoMinimo(senha) {
+        if (senha.length < constantes.SENHA_TAMANHO_MINIMO) {
+            throw new Error(mensagens.SENHA_MUITO_CURTA);
+        }
     }
-  }
+
+    static validarMaiuscula(senha) {
+        if (!/[A-Z]/.test(senha)) {
+            throw new Error(mensagens.SENHA_SEM_MAIUSCULA);
+        }
+    }
+
+    static validarMinuscula(senha) {
+        if (!/[a-z]/.test(senha)) {
+            throw new Error(mensagens.SENHA_SEM_MINUSCULA);
+        }
+    }
 }
 
 module.exports = { PasswordValidator };
