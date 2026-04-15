a92a442d59584b78563325d9fb300f837ce5bed3
RomuloJB <jordaobogoni@gmail.com>
2026-04-15 00:53:22 -0300

    test | deve rejeitar a senha sem letra minúscula

--- __test__/service/PasswordValidator.test.js
+++ __test__/service/PasswordValidator.test.js
@@ -2,13 +2,20 @@ const { PasswordValidator } = require('../src/service/PasswordValidator');
 const { mensagens } = require('../src/util/mensagens');
 
 describe('PasswordValidator', () => {
-  test('deve rejeitar senha com menos de 8 caracteres', () => {
-    expect(() => PasswordValidator.validar('Ab1!'))
-      .toThrow(mensagens.SENHA_MUITO_CURTA);
+    test('deve rejeitar senha com menos de 8 caracteres', () => {
+        expect(() => PasswordValidator.validar('Ab1!'))
+            .toThrow(mensagens.SENHA_MUITO_CURTA);
   });
 
-  test('deve rejeitar senha sem letra maiúscula', () => {
-    expect(() => PasswordValidator.validar('abc123!@'))
-      .toThrow(mensagens.SENHA_SEM_MAIUSCULA);
+    test('deve rejeitar senha sem letra maiúscula', () => {
+        expect(() => PasswordValidator.validar('abc123!@'))
+            .toThrow(mensagens.SENHA_SEM_MAIUSCULA);
   });
+
+    test('deve rejeitar senha sem letra minúscula', () => {
+        expect(() => PasswordValidator.validar('ABC123!@'))
+            .toThrow(mensagens.SENHA_SEM_MINUSCULA);
+    });
+
+    
 });
--- src/util/mensagens.js
+++ src/util/mensagens.js
@@ -1,6 +1,7 @@
 const mensagens = {
     SENHA_MUITO_CURTA: 'A senha deve ter no mínimo 8 caracteres.',
     SENHA_SEM_MAIUSCULA: 'A senha deve conter pelo menos uma letra maiúscula.',
+    SENHA_SEM_MINUSCULA: 'A senha deve conter pelo menos uma letra minúscula.',
 };
 
 module.exports = { mensagens };
