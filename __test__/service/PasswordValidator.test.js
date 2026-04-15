9c347fd98fedc846ca1e1eff37a10a748f843766
RomuloJB <jordaobogoni@gmail.com>
2026-04-15 01:00:55 -0300

    test | deve rejeitar a senha sem caractere especial

--- __test__/service/PasswordValidator.test.js
+++ __test__/service/PasswordValidator.test.js
@@ -5,12 +5,12 @@ describe('PasswordValidator', () => {
     test('deve rejeitar senha com menos de 8 caracteres', () => {
         expect(() => PasswordValidator.validar('Ab1!'))
             .toThrow(mensagens.SENHA_MUITO_CURTA);
-  });
+    });
 
     test('deve rejeitar senha sem letra maiúscula', () => {
         expect(() => PasswordValidator.validar('abc123!@'))
             .toThrow(mensagens.SENHA_SEM_MAIUSCULA);
-  });
+    });
 
     test('deve rejeitar senha sem letra minúscula', () => {
         expect(() => PasswordValidator.validar('ABC123!@'))
@@ -20,5 +20,10 @@ describe('PasswordValidator', () => {
     test('deve rejeitar senha sem número', () => {
         expect(() => PasswordValidator.validar('Abcdef!@'))
             .toThrow(mensagens.SENHA_SEM_NUMERO);
-});
+    });
+
+    test('deve rejeitar senha sem caractere especial', () => {
+        expect(() => PasswordValidator.validar('Abcdef12'))
+            .toThrow(mensagens.SENHA_SEM_CARACTERE_ESPECIAL);
+    });
 });
--- src/util/mensagens.js
+++ src/util/mensagens.js
@@ -3,6 +3,7 @@ const mensagens = {
     SENHA_SEM_MAIUSCULA: 'A senha deve conter pelo menos uma letra maiúscula.',
     SENHA_SEM_MINUSCULA: 'A senha deve conter pelo menos uma letra minúscula.',
     SENHA_SEM_NUMERO: 'A senha deve conter pelo menos um número.',
+    SENHA_SEM_CARACTERE_ESPECIAL: 'A senha deve conter pelo menos um caractere especial.',
 };
 
 module.exports = { mensagens };
