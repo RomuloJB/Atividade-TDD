9b5d754129dab676baf460a3706f8d52440d62f9
RomuloJB <jordaobogoni@gmail.com>
2026-04-15 00:49:08 -0300

    refactor | refatorando o passwordValidator para retornar as mensagens

--- src/service/PasswordValidator.js
+++ src/service/PasswordValidator.js
@@ -1,8 +1,20 @@
-function isPasswordValid(password) {
-    const hasMinLength = password.length >= 8;
-    const hasUppercase = /[A-Z]/.test(password);
+const { constantes } = require('../util/Constantes');
+const { mensagens } = require('../util/Mensagens');
 
-    return hasMinLength && hasUppercase;
+class PasswordValidator {
+  static validar(senha) {
+    const senhaMuitoCurta = senha.length < constantes.SENHA_TAMANHO_MINIMO;
+
+    if (senhaMuitoCurta) {
+      throw new Error(mensagens.SENHA_MUITO_CURTA);
+    }
+
+    if (!/[A-Z]/.test(senha)) {
+      throw new Error(mensagens.SENHA_SEM_MAIUSCULA);
+    }
+
+    return true;
+  }
 }
 
-module.exports = { isPasswordValid };
+module.exports = { PasswordValidator };
